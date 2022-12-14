1035ba80959c3f1fcf38d1f125f922bd096db8ab
Hugo Baca <86990271+HUGOXBACA@users.noreply.github.com>
2022-12-14 11:38:13 -0800

    Update elapsedtimefromlists.py

--- elapsedtimefromlists.py
+++ elapsedtimefromlists.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 ot = ['2022-12-22 20:55:33', '2022-12-23 17:22:33', '2022-12-23 16:44:33', '2012-12-24 15:33:12']
-ct = ['2022-12-22 23:55:33', '2022-12-23 19:21:33', '2022-12-23 19:00:00', '2012-12-24 19:33:12']
+ct = ['2022-12-22 23:55:33', '2022-12-23 19:21:33', '2022-12-25 19:00:00', '2012-12-24 19:33:12']
 time_format = "%Y-%m-%d %H:%M:%S"
 ##uncomment code to get the elapsed time for ot and ct [0]
 #this is the premise idea of the code, but the later uses no hardcoded values
@@ -15,6 +15,7 @@ time_format = "%Y-%m-%d %H:%M:%S"
 #try to use dates that span over multiple days, ie 12/01 to 12/9 and see what you get 
 
 ##the following is a list implementation 
+
 start = []
 stop = []
 
@@ -35,10 +36,13 @@ while i < 4:
     i+=1
 print(diff)
 
-
+avg = 0
+sum =0 
 for x in diff:
-     #following outputs in hours 
+    #this out puts in hours but can be modified
     x.total_seconds()/60/60
     print(x)
-avg = (diff[0]+diff[1]+diff[2]+diff[3]) / 3
-print("Avg elapsed times ", avg)
+    sum = sum + x.total_seconds()/60/60
+avg = round( sum / len(diff), 3)
+print("AVG hours elapsed:", avg)
+
